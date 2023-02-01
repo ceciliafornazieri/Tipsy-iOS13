9ce7a44c997e07f391b7cab831b3a2103bebd640
ceciliafornazieri <fornaziericecilia@gmail.com>
2023-02-01 18:04:56 -0300

    apaguei uma chaves que tinha a mais

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPcButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    
+    
+    @IBAction func tipChanged (_ sender: UIButton ) {
+        
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper){
+        
+    }
+    @IBAction func calculatePressed (_ sender: UIButton){
+        
+    }
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
